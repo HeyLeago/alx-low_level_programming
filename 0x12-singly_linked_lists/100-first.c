7c144f929db692c8c8abc383deaa1732693a7e22
HeyLeago <leagosebothoma@gmail.com>
2023-07-28 13:54:55 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *Description: prints a message before main
+ *
+ *Return: nothing
+*/
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
