f7aa2ec319cc42e5f35011e8b27380bb477c81fc
HeyLeago <leagosebothoma@gmail.com>
2023-08-01 19:33:41 -0700

    1. List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a listint_t
- * @h:listint_t to traverse
+ * listint_len - returns the number of elements in a linked lists
+ * @h: listint_t to traverse
  *
- * Return: number of nodes in the list
+ * Return: number of nodes in listint_t
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		num++;
 		h = h->next;
