8503b59bbd0ba69ab00338efbbf46f1b91893a21
HeyLeago <leagosebothoma@gmail.com>
2023-08-04 11:08:16 -0700

    task-1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,3 +5,26 @@
  * print_binary - prints the binary representation of a number
  * @n: number to print in binary
  */
+void print_binary(unsigned long int n)
+{
+	unsigned long int current;
+	int i = 63;
+	int total = 0;
+
+	while (i >= 0)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			putchar('1');
+			total++;
+		}
+		else if (total)
+			putchar('0');
+
+		i--;
+	}
+	if (!total)
+		putchar('0');
+}
