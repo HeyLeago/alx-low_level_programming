72bc66416c5c7a7d6a86a8d6839f5eb811daad44
HeyLeago <leagosebothoma@gmail.com>
2023-08-08 13:11:19 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,29 +11,26 @@
  *
  * Returns: 1 on success and -1 on failure
 */
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
-	if (filename == NULL)
-	{
+	int o, w, len = 0;
+
+	if (!filename)
 		return (-1);
+
+	if (text_content)
+	{
+		while (len <= text_content[len])
+			len++;
 	}
 
-	int fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	if (fd == -1)
-	{
+	if (o == -1 || w == -1)
 		return (-1);
-	}
 
-	size_t len = (text_content != NULL) ? strlen(text_content) : 0;
-	size_t w = write(fd, text_content, len);
+	close(o);
 
-	if (w != (ssize_t)len)
-	{
-		close(fd);
-		return (-1);
-	}
-
-	close(fd);
 	return (1);
 }
