5bdd43eb9d9fca5fe268372a4438d22858949331
HeyLeago <leagosebothoma@gmail.com>
2023-08-08 08:33:54 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,8 +10,10 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
-
+	int fd = 0;
+       	int w = 0;
+	int len = 0;
+	
 	if (!filename)
 		return (-1);
 
@@ -21,13 +23,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	w = write(fd, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	close(o);
+	close(fd);
 
 	return (1);
 }
